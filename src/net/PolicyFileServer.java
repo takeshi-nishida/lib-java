a3c39f91544aad8a90c74fc24695a3e9e61503e6
Takeshi Nishida <takeshi-nishida@users.noreply.github.com>
2012-12-24 23:05:10 +0900

    PolicyFileServer is now working properly.

--- src/net/PolicyFileServer.java
+++ src/net/PolicyFileServer.java
@@ -1,59 +1,94 @@
 package net;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import net.chat.Constants;
-import util.StringUtilities;
 
 /**
  *
  * @author tnishida
  */
-public class PolicyFileServer implements Runnable, StringListener {
+public class PolicyFileServer implements Runnable {
 
-    public static final String policyFileRequest = "<policy-file-request/>";
-    private static final String policyFileFormat = "<cross-domain-policy><allow-access-from domain=\"*\" to-ports=\"%s\"/></cross-domain-policy>";
+  public static final String policyFileRequest = "<policy-file-request/>";
+  private static final String policyFileFormat = "<cross-domain-policy><allow-access-from domain=\"*\" to-ports=\"%s\"/></cross-domain-policy>\0";
+  private int port;
+  private String policyFileText;
+  private byte[] policyFileData;
 
-    private int port;
-    private String policyFileText;
+  public PolicyFileServer(int port, int... toports) {
+    this.port = port;
+    this.policyFileText = getPolicyFileText(toports);
+    try {
+      this.policyFileData = policyFileText.getBytes("UTF-8");
+    } catch (UnsupportedEncodingException ex) {
+    }
+  }
 
-    public PolicyFileServer(int port, int ... toports){
-        this.port = port;
-        this.policyFileText = getPolicyFileText(toports);
+  public static void start(int port, int... toports) {
+    PolicyFileServer server = new PolicyFileServer(port, toports);
+    Thread thread = new Thread(server);
+    thread.start();
+  }
+
+  public static String getPolicyFileText(int... toports) {
+    StringBuilder builder = new StringBuilder();
+    for (int i = 0; i < toports.length; i++) {
+      builder.append(toports[i]);
+      if (i < toports.length - 1) {
+        builder.append(",");
+      }
     }
 
-    public static void start(int port, int ... toports) {
-        PolicyFileServer server = new PolicyFileServer(port, toports);
-        Thread thread = new Thread(server);
-        thread.start();
+    return String.format(policyFileFormat, builder.toString());
+  }
+
+  @Override
+  public void run() {
+    try {
+      System.out.println("Starting policy file server at: " + port);
+      ServerSocket ss = new ServerSocket(port);
+      while (true) {
+        Socket socket = ss.accept();
+        PolicyFileSender sender = new PolicyFileSender(socket);
+        new Thread(sender).start();
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  class PolicyFileSender implements Runnable {
+
+    private Socket socket;
+
+    public PolicyFileSender(Socket socket) {
+      this.socket = socket;
     }
 
-    public static String getPolicyFileText(int... toports){
-        return String.format(policyFileFormat, StringUtilities.join(",", toports)) + "u0000";
-    }
-
+    @Override
     public void run() {
-        try {
-            ServerSocket ss = new ServerSocket(port);
-            while (true) {
-                Socket socket = ss.accept();
-                StringSession session = new StringSession(this, socket);
-                session.setCharsetName(Constants.defaultCharset);
-                session.startup();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+      try (InputStreamReader reader = new InputStreamReader(socket.getInputStream());
+              OutputStream os = socket.getOutputStream()) {
+        char[] buffer = new char[32];
+        int offset = 0;
+        do{
+          offset += reader.read(buffer, offset, buffer.length - offset);
+        }while(offset < buffer.length && buffer[offset] != '\0') ;
+        String s = new String(buffer);
 
-    public void received(StringSession session, String s) {
-        if(s.startsWith(policyFileRequest)){
-            session.send(policyFileText);
+        if (s.startsWith(policyFileRequest)) {
+          System.out.println("Policy file requested.");
+          os.write(policyFileData);
+          os.flush();
+          System.out.println("Policy file sent.");
         }
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
     }
-
-    public void cleaned(StringSession session, boolean abnormally) {
-        // do nothing
-    }
+  }
 }
